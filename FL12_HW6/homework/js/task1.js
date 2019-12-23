a32017205d843dbf90c78849b73bcdb767c3b1f8
Bohdan Pantiukhov <gingermanymph@gmail.com>
2019-12-23 19:55:00 +0200

    task2 - deleted unnecessary call Math.sqrt

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -12,7 +12,7 @@ if (a !== 0 || b !== 0 || c !== 0) {
     if (discriminant > 0) {
         x1 = (-b + Math.sqrt(discriminant)) / (two * a);
         x2 = (-b - Math.sqrt(discriminant)) / (two * a);
-        console.log(`Discriminant = ${Math.sqrt(discriminant)}`);
+        console.log(`Discriminant = ${discriminant}`);
         console.log(`x1 = ${x1}; x2 = ${x2}`)
     } else if (discriminant === 0) {
         x = -b / (two * a);
