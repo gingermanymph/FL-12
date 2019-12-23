2e55cfcdae490534ec8bed53a2d18f10178d9f28
Bohdan Pantiukhov <gingermanymph@gmail.com>
2019-12-23 19:18:55 +0200

    tasks done

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const two = 2;
+const four = 4;
+
+let a = parseInt(prompt('Enter number a'));
+let b = parseInt(prompt('Enter number b'));
+let c = parseInt(prompt('Enter number c'));
+let x, x1, x2, discriminant;
+
+if (!isNaN(a) || !isNaN(b) || !isNaN(c)) {
+    if (a !== 0 || b !== 0 || c !== 0) {
+        discriminant = Math.pow(b, two) - four * a * c;
+        if (discriminant > 0) {
+            x1 = (-b + Math.sqrt(discriminant)) / (two * a);
+            x2 = (-b - Math.sqrt(discriminant)) / (two * a);
+            console.log(`Discriminant = ${Math.sqrt(discriminant)}`);
+            console.log(`x1 = ${x1}; x2 = ${x2}`)
+        } else if( discriminant === 0) {
+            x = -b / (two * a);
+            console.log(`x = ${x}`)
+        }else{       
+            console.log('No solution')
+        } 
+    } else {
+        console.log('Invalid input data');
+    }
+} else {
+    console.log('Invalid input data');
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,29 @@
 // Your code goes here
+let headA = parseInt(prompt('Enter head A'));
+let headB = parseInt(prompt('Enter head B'));
+let headC = parseInt(prompt('Enter head C'));
+let sideAB, sideBC, sideCA;
+
+if (!isNaN(headA) || !isNaN(headB) || !isNaN(headC)) {
+    if (headA > 0 || headB > 0 || headC > 0) {
+        sideAB = headA + headB;
+        sideBC = headB + headC;
+        sideCA = headC + headA;
+
+        if (sideAB > headC || sideBC > headA || sideCA > headB) {
+            if (sideAB === sideBC && sideBC === sideCA) {
+                console.log('Equilateral triangle')
+            } else if (sideAB !== sideBC && sideBC !== sideCA) {
+                console.log('Scalene triangle')
+            } else {
+                console.log('Isosceles triangle')
+            }
+        } else {
+            console.log('Triangle doesn\'t exist')
+        }
+    } else {
+        console.log('A triangle must have 3 sides with a positive definite length');
+    }
+} else {
+    console.log('Input values should be ONLY numbers');
+}
