34b934f2b352cfd731852d1f6aac6e3a7f9cb8b9
Bohdan Pantiukhov <gingermanymph@gmail.com>
2019-12-30 18:51:42 +0200

    Task #5 - deleted method split

--- FL12_HW8/homework/js/makeNumber.js
+++ FL12_HW8/homework/js/makeNumber.js
@@ -1,4 +1,4 @@
-const makeNumber = (str) => str.split('').filter(a => !isNaN(a)).join('');
+const makeNumber = str => Array.from(str).filter(a => !isNaN(a)).join('');
 
 makeNumber('erer384jjjfd123'); //=>384123
 makeNumber('123098h76gfdd'); //=>12309876
