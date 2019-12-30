db99d71975e39ebe66d705aac120ac3f5024168a
Bohdan Pantiukhov <gingermanymph@gmail.com>
2019-12-30 20:36:15 +0200

    Task #3 fixed reduce error with empty arg

--- FL12_HW8/homework/js/getMin.js
+++ FL12_HW8/homework/js/getMin.js
@@ -1,3 +1,3 @@
-const getMin = (...args) => args.reduce((min, last) => last < min ? last : min );
+const getMin = (...args) => args.reduce((min, last) => last < min ? last : min, args[0]);
 
 getMin(3, 0, -3); 
