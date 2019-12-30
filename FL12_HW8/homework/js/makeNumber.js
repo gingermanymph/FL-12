1b30b42b824b98fed3f40d64b548200d9162b47d
Bohdan Pantiukhov <gingermanymph@gmail.com>
2019-12-31 00:20:09 +0200

    Task #4 fixed "Cannot convert undefined or null to object" error with empty arg

--- FL12_HW8/homework/js/makeNumber.js
+++ FL12_HW8/homework/js/makeNumber.js
@@ -1,4 +1,4 @@
-const makeNumber = str => Array.from(str).filter(a => !isNaN(a)).join('');
+const makeNumber = str => str ? Array.from(str).filter(a => !isNaN(a)).join('') : undefined;
 
 makeNumber('erer384jjjfd123'); //=>384123
 makeNumber('123098h76gfdd'); //=>12309876
