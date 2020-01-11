25b663cebc5828a3963a43a34aecd46e57897c35
Bohdan Pantiukhov <gingermanymph@gmail.com>
2020-01-11 19:08:20 +0200

    9-10 tasks done

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -69,15 +69,35 @@ const getArrayOfKeys = (arr, key) => {
 const substitute = arr => mapArray(arr, el => el < 30 ? '*' : el);
 
 // task #9
+const date = new Date(2019, 0, 2);
+const getPastDay = (date, number) => {
+    const MS_IN_DAY = 86400000;
+    date = date.getTime() - MS_IN_DAY * number;
+    return new Date(date).getDate();
+}
 
 // task #10
-
+const formatDate = date => {
+    return `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+}
 // Calls
-convert('1', 2, 3, '4'); // [1, '2', '3', 4]
-executeforEach([1, 2, 3], el => console.log(el * 2)); // logs 2 4 6
-mapArray([2, '5', 8], el => el + 3); // returns [5, 8, 11]
-filterArray([2, 5, 8], el => el % 2 === 0); // returns [2, 8]
-flipOver('hey world'); // 'dlrow yeh'
-makeListFromRange([2, 7]); // [2, 3, 4, 5, 6, 7]
-getArrayOfKeys(actors, 'name'); // ['tommy', 'lee']
-substitute([58, 14, 48, 2, 31, 29]); // [58, '*', 48, '*', 31, '*']
+console.log(convert('1', 2, 3, '4'));
+ // [1, '2', '3', 4]
+console.log(executeforEach([1, 2, 3], el => console.log(el * 2)));
+// logs 2 4 6
+console.log(mapArray([2, '5', 8], el => el + 3));
+// returns [5, 8, 11]
+console.log(filterArray([2, 5, 8], el => el % 2 === 0));
+// returns [2, 8]
+console.log(flipOver('hey world'));
+// 'dlrow yeh'
+console.log(makeListFromRange([2, 7]));
+// [2, 3, 4, 5, 6, 7]
+console.log(getArrayOfKeys(actors, 'name'));
+// ['tommy', 'lee']
+console.log(substitute([58, 14, 48, 2, 31, 29]));
+// [58, '*', 48, '*', 31, '*']
+console.log(getPastDay(date, 1));
+// 1, (1 Jan 2019)
+console.log(formatDate(new Date('6/15/2018 09:15:00')));
+// "2018/6/15 09:15"
